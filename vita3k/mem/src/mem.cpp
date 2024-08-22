f84a8f86100e6ebd17d525d7c09d2cd51f996947
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-08-22 18:56:25 +0800

    Update mem.cpp

--- vita3k/mem/src/mem.cpp
+++ vita3k/mem/src/mem.cpp
@@ -75,7 +75,7 @@ bool init(MemState &state, const bool use_page_table) {
 //    LOG_DEBUG("Default virtual Memory size: {} Bytes", TOTAL_MEM_SIZE);
 //    LOG_DEBUG("Default page size: {} Bytes", STANDARD_PAGE_SIZE);
     uint64_t mem_size_tmp = SDL_GetSystemRAM();
-    mem_size_tmp = static_cast<uint64_t>( mem_size_tmp - 1000 ) * 1000000);
+    mem_size_tmp = static_cast<uint64_t>(mem_size_tmp / 3) * 1000000;
 //    LOG_DEBUG("Custom memory size set: {} Bytes", mem_size_tmp);
     if(mem_size_tmp < TOTAL_MEM_SIZE){
        LOG_DEBUG("Virtual Memory size too low!, using default value!");
