37282c7f58f9885dc5d35b66a12361fb014ae729
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-11-06 00:12:01 +0800

    Update trophy_collection.cpp

--- vita3k/gui/src/trophy_collection.cpp
+++ vita3k/gui/src/trophy_collection.cpp
@@ -743,6 +743,7 @@ void draw_trophy_collection(GuiState &gui, EmuEnvState &emuenv) {
             }
         }
     }
+    ImGui::ScrollWhenDragging();
     ImGui::EndChild();
 
     ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 10.f * SCALE.x);
